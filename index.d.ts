23581112bad84ff800069cd125f930e1b2cc54fc
Dimitri Benin <dimitri.benin@gmail.com>
2019-03-12 23:29:23 +0100

    Add TypeScript definition

---
+++ index.d.ts
@@ -0,0 +1,5 @@
+/**
+ * Check when the DOM is loaded like [`DOMContentLoaded`](https://developer.mozilla.org/en/docs/Web/Events/DOMContentLoaded).
+ */
+declare const domLoaded: Promise<void>;
+export default domLoaded;
